be4517e697eba2dd4580dbb25325806b1b7db364
keithcurtis1 <keithcurtis@gmail.com>
2019-08-04 17:44:13 -0700

    Update Inspiration.js

--- Inspiration.js
+++ Inspiration.js
@@ -1,47 +1,68 @@
 on('chat:message', (msg) => {
-            if ('api' === msg.type && /!inspire\b/i.test(msg.content) && msg.selected) {
+            
+    //const version = '0.0.2';
+            
+    if ('api' === msg.type && /!inspire\b/i.test(msg.content) && msg.selected) {
+        log(msg);
+        //get parameter
+        let cardAction = msg.content.split(" --")[1];
 
-                //getid of deck
-                var inspirationDeck = findObjs({
-                    _type: "deck",
-                    name: "Inspiration",
-                })[0];
-                //test if deck exists
-                if (inspirationDeck) {
 
-                    var deckID = inspirationDeck.get("_id");
+        //getid of deck
+        var inspirationDeck = findObjs({
+            _type: "deck",
+            name: "Inspiration",
+        })[0];
 
-                    //get id of card
-                    let cardid = drawCard(deckID);
+        //test if deck exists
+        if (inspirationDeck) {
 
-                    // get playerId of Token controller
-                    //assign selected token to a variable
-                    let token = getObj(msg.selected[0]._type, msg.selected[0]._id);
-                    
-                    //assign associated character to a variable
-                    if (token.get('represents')) {
-                        let character = getObj("character", token.get('represents'));
+            var deckID = inspirationDeck.get("_id");
 
-                        //Get owner IDs of each
-                        tokenOwner = (token.get('controlledby').split(',')[0]);
-                        characterOwner = (character.get('controlledby').split(',')[0]);
-                        // If the token represents a character, get the character's controller, otherwise the token's
-                        let ownerid = (token.get('controlledby').split(',')[0]);
+            //get id of card
+            let cardid = drawCard(deckID);
 
-                        if (character) {
-                            ownerid = (character.get('controlledby').split(',')[0]);
-                        }
-                        // give card to player
-                        if (ownerid!== 'all'){
-                        giveCardToPlayer(cardid, ownerid);
-                        } else{
-                        sendChat('Inspire', '/w gm If a token is controlled by All Players, an individual player must be specified as first in the list.');
-                        }
-                    } else {
-                        sendChat('Inspire', '/w gm This token does not represent a player. Only players get Inspiration.');
-                    }
-                } else {
-                    sendChat('Inspire', '/w gm Create a deck named Inspiration.It must be an infinite deck of one card only.');
+            // get playerId of Token controller
+            //assign selected token to a variable
+            let token = getObj(msg.selected[0]._type, msg.selected[0]._id);
+
+            //assign associated character to a variable
+            if (token.get('represents')) {
+                let character = getObj("character", token.get('represents'));
+
+                //Get owner IDs of each
+                tokenOwner = (token.get('controlledby').split(',')[0]);
+                characterOwner = (character.get('controlledby').split(',')[0]);
+                // If the token represents a character, get the character's controller, otherwise the token's
+                let ownerid = (token.get('controlledby').split(',')[0]);
+
+                if (character) {
+                    ownerid = (character.get('controlledby').split(',')[0]);
                 }
+                // give card to player
+                if (ownerid !== 'all') {
+
+                    switch (cardAction) {
+                        case 'give':
+                            giveCardToPlayer(cardid, ownerid);
+                            break;
+                        case 'take':
+                            takeCardFromPlayer(ownerid, cardid);
+                            break;
+                        default:
+                    sendChat('Inspire', '/w gm '+cardAction+' is not a valid parameter Please use --give or --take.');
+                                                break;
+
+                    }
+
+                } else {
+                    sendChat('Inspire', '/w gm If a token is controlled by All Players, an individual player must be specified as first in the list.');
+                }
+            } else {
+                sendChat('Inspire', '/w gm This token does not represent a player. Only players get Inspiration.');
             }
-            });
+        } else {
+            sendChat('Inspire', '/w gm Create a deck named Inspiration.It must be an infinite deck of one card only.');
+        }
+    }
+});
