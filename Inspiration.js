8fbbc2f3c978e0f81f59df16764e3c074d53c485
keithcurtis1 <keithcurtis@gmail.com>
2019-08-05 21:13:09 -0700

    Update Inspiration.js

--- Inspiration.js
+++ Inspiration.js
@@ -1,6 +1,6 @@
 on('chat:message', (msg) => {
     if ('api' === msg.type && /!inspire\b/i.test(msg.content) && msg.selected) {
-        log(msg);
+        //        log(msg);
         //get parameter and use default of 'give' if parameter is missing or malformed
         let cardAction = msg.content.split(/\s+--/)[1];
         if ((cardAction !== "give" && cardAction !== "take") || cardAction === false) {
@@ -22,39 +22,55 @@ on('chat:message', (msg) => {
 
             // get playerId of Token controller
             //assign selected token to a variable
-            let token = getObj(msg.selected[0]._type, msg.selected[0]._id);
 
-            //assign associated character to a variable
-            if (token.get('represents')) {
-                let character = getObj("character", token.get('represents'));
+            log('number of tokens selected is ' + msg.selected.length);
+            if (msg.selected.length <= 1) {
 
-                //Get owner IDs of each -- Not needed at this point
-                //tokenOwner = (token.get('controlledby').split(',')[0]);
-                //characterOwner = (character.get('controlledby').split(',')[0]);
-                // If the token represents a character, get the character's controller, otherwise the token's
-                let ownerid = (token.get('controlledby').split(',')[0]);
+                let token = getObj(msg.selected[0]._type, msg.selected[0]._id);
 
-                if (character) {
-                    ownerid = (character.get('controlledby').split(',')[0]);
-                }
-                // give card to player
-                if (ownerid !== 'all') {
+                //assign associated character to a variable
+                if (token.get('represents')) {
+                    let character = getObj("character", token.get('represents'));
 
-                    switch (cardAction) {
-                        case 'take':
-                            takeCardFromPlayer(ownerid, {cardid: cardid});
-                            break;
-                        default:
-                            giveCardToPlayer(cardid, ownerid);
-                            break;
+                    //Get owner IDs of each -- Not needed at this point
+                    //tokenOwner = (token.get('controlledby').split(',')[0]);
+                    //characterOwner = (character.get('controlledby').split(',')[0]);
+                    // If the token represents a character, get the character's controller, otherwise the token's
+                    let ownerids = (token.get('controlledby').split(','));
 
+
+                    if (character) {
+                        ownerids = (character.get('controlledby').split(','));
                     }
+                    //reduces to one ownerid that is not ['all']
+                    ownerid = ownerids.filter(s => s !== 'all')[0];
 
+
+                    // give card to player
+                    // If the ownerid is undefined (no valid controller) perform the transaction
+                    if (ownerid) {
+
+                        switch (cardAction) {
+                            case 'take':
+                                takeCardFromPlayer(ownerid, {
+                                    cardid: cardid
+                                });
+                                break;
+                            default:
+                                giveCardToPlayer(cardid, ownerid);
+                                break;
+
+                        }
+
+                    } else {
+
+                        sendChat('Inspire', '/w gm If a token represents a character controlled by \'All Players\', an individual player must be also be specified. If there are multiple controllers, only the first will get inspiration.');
+                    }
                 } else {
-                    sendChat('Inspire', '/w gm If a token is controlled by All Players, an individual player must be specified as first in the list.');
+                    sendChat('Inspire', '/w gm This token does not represent a player character. Only players get Inspiration.');
                 }
             } else {
-                sendChat('Inspire', '/w gm This token does not represent a player. Only players get Inspiration.');
+                sendChat('Inspire', '/w gm Please select only one token. It must represent player-controlled character.');
             }
         } else {
             sendChat('Inspire', '/w gm Create a deck named Inspiration.It must be an infinite deck of one card only.');
